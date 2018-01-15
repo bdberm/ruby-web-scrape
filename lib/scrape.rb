c14ffc6d5d64747f618ba1c162a62982fcd5492b
Benjamin Berman <bdberm@gmail.com>
2018-01-15 09:18:23 -0500

    grabbing model and rating data

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -9,5 +9,9 @@ count = doc.at_css('.count').content[1...-1].to_i
 
 list_items.each do |list_item|
   brand_content = list_item.attribute('data-brand').content
-  puts(brand_content[brand_content.index(":")+2..-3])
+  title_content = list_item.attribute('data-title').content
+  num_ratings = list_item.attribute('data-review-count').content.to_i
+  average_rating = list_item.attribute('data-average-rating').content.to_f
+  # puts(brand_content[brand_content.index(":")+2..-3])
+  # puts(average_rating)
 end
