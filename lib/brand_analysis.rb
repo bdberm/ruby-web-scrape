abf22868fe33379356b5af403dc9d1cbbe4df35e
Benjamin Berman <bdberm@gmail.com>
2018-01-17 10:46:49 -0500

    add scrape for 1/17

--- lib/brand_analysis.rb
+++ lib/brand_analysis.rb
@@ -5,7 +5,9 @@ def find_brand_coverage_all_search_terms(start_date_str, end_date_str)
   db = SQLite3::Database.open('lib/db_files/best_buy_rankings.db')
 
 
+
   result = db.execute(<<-SQL)
+
     SELECT * FROM best_buy_rankings
   SQL
 
